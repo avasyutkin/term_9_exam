f273d96550bf0e4ab46376339b62a5b455fa3fb2
Alexander <43503189+avasyutkin@users.noreply.github.com>
2023-01-26 12:39:23 +0300

    Update main_test.py

--- unittest/main_test.py
+++ unittest/main_test.py
@@ -6,14 +6,9 @@ data_output = {0, 1, 2, 4}
 
 
 class MyTestCase(unittest.TestCase):
-    def test_positive(self):
+    def test(self):
         result = main.quadratic_residues(data_input[0])
         self.assertEqual(result, data_output)
-
-    def test_negative(self):
-        result = main.quadratic_residues(data_input[1])
-        self.assertEqual(result, data_output)
-
 
 
 if __name__ == '__main__':
